3d577aefc6f6a05c0b2d091efbfdcf0891467534
smucon-dev <dev@smucon.com>
2022-03-29 11:32:46 +0200

    Initial commit

---
+++ lib/serverless-chat-app-backend-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ServerlessChatAppBackendStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'ServerlessChatAppBackendQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
